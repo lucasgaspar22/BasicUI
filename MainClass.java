7ae5dff9a052bfddbadd77e0164e248486fe2c38
Lucas Gaspar <lucasprgaspar@gmail.com>
2017-12-19 10:34:24 -0200

    MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import View.MainFrame;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		MainFrame myFrame = new MainFrame();
+		myFrame.setVisible(true);
+	}
+
+}
